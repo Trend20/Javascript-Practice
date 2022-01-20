c80d8cd83698e87ee0d2c702c71f14317052aa5c
DevEnock <enockomondi305@gmail.com>
2022-01-20 07:54:16 +0300

    arrays

---
+++ Array/array.js
@@ -0,0 +1,23 @@
+let numbers = [1, 45, 8, 9, 4, 5];
+
+// find the sum of all array elements
+let sumOfArray = 0;
+
+// loop through the array elements
+
+for(let i = 0; i < numbers.length; i++){
+  console.log(numbers[i]);
+   sumOfArray += number[i];
+}
+
+
+// concat
+function showConcat(){
+  let treeNames = ['moon', 'base'];
+let nums = [2, 3, 5, 7];
+
+combinedDetails = treeNames.concat(nums);
+console.log(combinedDetails);
+}
+
+showConcat();
