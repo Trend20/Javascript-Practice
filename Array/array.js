d7eb46acf2de8a47eb19709364decec023abb69f
DevEnock <enockomondi305@gmail.com>
2022-01-23 11:28:58 +0300

    updating arrays

--- Array/array.js
+++ Array/array.js
@@ -7,7 +7,7 @@ let sumOfArray = 0;
 
 for(let i = 0; i < numbers.length; i++){
   console.log(numbers[i]);
-   sumOfArray += number[i];
+   sumOfArray += numbers[i];
 }
 
 
@@ -21,3 +21,37 @@ console.log(combinedDetails);
 }
 
 showConcat();
+
+// ARRAY OPERATIONS
+
+function performArrayOperations(){
+  // creating array
+let studentMarks = [20, 30 ,50, 60, 70];
+// find the sum of the array
+
+let sum =0;
+for(let i =0; i< studentMarks.length; i++){
+  sum += studentMarks[i];
+}
+console.log(sum);
+
+
+// create array using the constructor
+let marks = new Array(20);
+marks.push(30, 40, 50, 80, 90);
+console.log(marks);
+
+// creating arrays from string
+let greeting = 'Hello! How are you doing?';
+const greetingArray = greeting.split(' ');
+console.log(greetingArray);
+
+// loop through the array
+
+for(let i =0; i<greetingArray.length; i++){
+  console.log(greetingArray[i]);
+}
+
+}
+
+performArrayOperations();
